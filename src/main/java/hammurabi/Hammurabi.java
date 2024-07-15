fefd7c4954412531b10136f86bf4b0bd3f2fe840
coreyeross25 <coreyeross12@gmail.com>
2024-07-14 22:54:16 -0400

    tests run, passed 4/9

--- src/main/java/hammurabi/Hammurabi.java
+++ src/main/java/hammurabi/Hammurabi.java
@@ -1,5 +1,6 @@
 package hammurabi;
 
+import java.util.InputMismatchException;
 import java.util.Random;         // imports go here
 import java.util.Scanner;
 
@@ -15,16 +16,29 @@ public class Hammurabi {         // must save in a file named hammurabi.Hammurab
     void playGame() {
         // declare local variables here: grain, population, etc.
         int totalDeaths = 0, percentDied = 0, year = 0, population = 95, stores = 2800, immigrants = 5, deaths,
-                harvest = 3000, yield = 3, acres = harvest / yield, eaten = harvest - stores, landPrice, fullPeople, temp;
+                harvest = 3000, yield = 3, acres = harvest / yield, eaten = harvest - stores, landPrice = 19, fullPeople, temp;
 
-
+        askHowManyAcresToBuy(landPrice, stores);
 
         // statements go after the declarations
     }
 
     //other methods go here
-    public int askHowManyAcresToBuy(int price, int bushels){
-        return 0;
+    public int askHowManyAcresToBuy(int price, int bushels) {
+        int acresTryingToBuy;
+        while (true) {
+            System.out.print("How many acres would you like to buy?");
+            acresTryingToBuy = 0;
+            try {
+                acresTryingToBuy = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("\"" + scanner.next() + "\" isn't a number!");
+            }
+            if (acresTryingToBuy > 0) {
+                break;
+            }
+        }
+        return acresTryingToBuy;
     }
 
     public int askHowManyAcresToSell(int acresOwned){
@@ -47,12 +61,24 @@ public class Hammurabi {         // must save in a file named hammurabi.Hammurab
     }
 
     public int starvationDeaths(int population, int bushelsFedToPeople) {
+        int neededForSurvival = population * 20;
+        if (bushelsFedToPeople < neededForSurvival){
+            return population - (bushelsFedToPeople / 20);
+        }
         return 0;
     }
 
     public boolean uprising(int population, int howManyPeopleStarved) {
-        return true;
+        double percentStarved = (double) howManyPeopleStarved / population * 100;
+
+        if (percentStarved > 45.00) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
+
 
     public int immigrants(int population, int acresOwned, int grainInStorage) {
         return 0;
